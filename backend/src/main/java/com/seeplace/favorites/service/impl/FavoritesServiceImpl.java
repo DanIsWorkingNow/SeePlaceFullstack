e6e6644c223235d3a0e6d39ce56a992fef04c6d6
DanIsWorkingNow <danialsswork@gmail.com>
2025-08-21 16:12:49 +0800

    Corrected the import for BigDecimal on service page

--- backend/src/main/java/com/seeplace/favorites/service/impl/FavoritesServiceImpl.java
+++ backend/src/main/java/com/seeplace/favorites/service/impl/FavoritesServiceImpl.java
@@ -8,6 +8,7 @@ import com.seeplace.favorites.service.FavoritesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.math.BigDecimal;
 
 import java.util.List;
 import java.util.stream.Collectors;
